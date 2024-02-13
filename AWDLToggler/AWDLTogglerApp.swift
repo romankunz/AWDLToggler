b634fa842959ab7ac290da6cfc3350c9dcb24e07
Roman M. A. Kunz <krom@krom.ch>
2024-02-13 11:43:10 +0100

    Initial Commit

---
+++ AWDLToggler/AWDLTogglerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AWDLTogglerApp.swift
+//  AWDLToggler
+//
+//  Created by Roman Kunz on 13.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct AWDLTogglerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
